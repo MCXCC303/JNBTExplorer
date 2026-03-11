1002cde33f72ca9d4a4e923368b8315413bd868f
MCXCC <minecraftmcxcc@gmail.com>
2026-03-12 00:56:59 +0800

    Updated build config

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
+COMMIT_HASH=$(git rev-parse --short HEAD)
+JAR_FILE=JNBTExplorer+git-$COMMIT_HASH.jar
+RELEASE_JAR_FILE=JNBTExplorer.jar
+echo "jar_file=$JAR_FILE"			>> $GITHUB_OUTPUT
+echo "commit_hash=$COMMIT_HASH"			>> $GITHUB_OUTPUT
+echo "release_jar_file=$RELEASE_JAR_FILE" 	>> $GITHUB_OUTPUT
 echo "Building JNBTExplorer JAR file..."
 
-# Create bin directory if not exists
 mkdir -p bin
 
-# Compile all Java files
 echo "Compiling Java files..."
 javac -encoding UTF-8 -d bin -sourcepath src src/com/MCXCC/JNBTExplorer/JNBTExplorerMain.java
 if [ $? -ne 0 ]; then
@@ -13,14 +16,16 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Create JAR file
 echo "Creating JAR file..."
-jar cvfm JNBTExplorer.jar MANIFEST.MF -C bin .
+jar cvfm $JAR_FILE MANIFEST.MF -C bin .
 if [ $? -ne 0 ]; then
     echo "JAR creation failed!"
     exit 1
 fi
 
+cp $JAR_FILE $RELEASE_JAR_FILE
+
 echo ""
-echo "Build successful! Created JNBTExplorer.jar"
-echo "You can run it with: java -jar JNBTExplorer.jar"
+echo "Build successful! Created $RELEASE_JAR_FILE"
+echo "You can run it with: java -jar $RELEASE_JAR_FILE"
+echo ""
